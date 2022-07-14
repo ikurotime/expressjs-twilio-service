f30bb92b518190c04b1515270a9dbbc2d5bd3a19
ikurotime <davidhuertasortiz@gmail.com>
2022-07-15 00:14:09 +0200

    🚀 Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,145 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 import bodyParser from "body-parser";
 import express from "express";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseToken = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceRole = process.env.SUPABASE_SERVICE_ROLE;
+
+const supabase = createClient(supabaseUrl!, supabaseServiceRole!);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKey = process.env.TWILIO_API_KEY;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioApiSecret = process.env.TWILIO_API_SECRET;
+
+const client = require('twilio')(accountSid, authToken);
+const AccessToken = require('twilio').jwt.AccessToken;
+const ChatGrant = AccessToken.ChatGrant;
+
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3333;
+const addToDatabase = async (database: string,query: any) => {
+  const {data,error} = await supabase.from(database).insert([ query ])
+  console.log(data)
+  console.log(error)
+}
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
+// Create server
+app.get("/create-server", async (req, res) => {
+  const access_token = req.headers?.access_token as string
+  if(!access_token.startsWith('anonymous')){
+    supabase.auth.setAuth(access_token)
+  }
+  
+  const { data,error } = await supabase
+  .from('servers')
+  .select('friendly_name').eq('friendly_name', req.headers?.friendlyname)
+  console.log(data)
+  console.log(error)
+  if(data!.length > 0) return res.status(403).send('Server already exists')
+
+  let identity:string;
+  console.log(access_token)
+  if (access_token.startsWith('anonymous')) {
+		identity = access_token.split('_')[1];
+	} else {
+		const { data } = await supabase.auth.api.getUser(access_token);
+		identity = data!.user_metadata.full_name;
+	}
+
+  client.conversations.v1.services.create({friendlyName: req.headers?.friendlyname, uniqueName: req.headers?.uniquename})
+  .then(async (service: { sid: string; }) => {
+    try {
+      addToDatabase('servers',{ friendly_name: req.headers.friendlyname,unique_name: req.headers.uniquename, SID: service.sid,created_by: req.headers.uid })
+    } catch (error) {
+      res.send(error)
+    }
+    try {
+      client.conversations.v1.services(service.sid)
+      .conversations
+      .create({uniqueName:'general',friendlyName:'general'}).then(async (conversation: { sid: string; }) => {
+        try {
+          await client.conversations.v1.services(service.sid)
+          .conversations(conversation.sid)
+          .participants
+          .create({identity})
+          addToDatabase('channels',  { channel_sid: conversation.sid,created_by:req.headers?.uid, server_sid: service.sid,channel_friendly_name: 'general' })
+          addToDatabase('members', { user_id: req.headers.uid, server_sid: service.sid })
+          addToDatabase('channel_members', { user_id: req.headers.uid, channel_id: conversation.sid })
+        res.send({ serverSid: service.sid, conversation: conversation })
+
+        } catch (error) {
+          res.send(error)
+        }
+      }
+      )
+    } catch (error) {
+      res.send(error)
+    }
+  }); 
 });
+
+// Get access-token
+app.get("/get-access-token", async (req, res) => {
+  const jwt = req.headers?.jwt as string
+	const SERVICE_SID = req.headers?.serversid as string
+
+	if (jwt == null) {
+		return res.status(401)
+	}
+	let identity;
+	/* We try to get the identity from the jwt in order to make a new jwt with Twilio
+		 if the user is not anonymous we can get the identity directly from Supabase
+	*/
+	if (jwt.startsWith('anonymous')) {
+		identity = jwt.split('_')[1];
+	} else {
+		const { data } = await supabase.auth.api.getUser(jwt);
+		identity = data!.user_metadata.full_name;
+	}
+
+	if (identity == null )return res.status(401)
+
+	const accessToken = new AccessToken(accountSid, twilioApiKey, twilioApiSecret, {
+		identity
+	});
+
+	const conversationGrant = new ChatGrant({
+		serviceSid: SERVICE_SID
+	});
+	accessToken.addGrant(conversationGrant);
+
+  return res.status(200).send({
+    accessToken: accessToken.toJwt(),
+    identity
+  });
+})
+// Get all servers
+app.get("/get-all-servers", async (req, res) => {
+  client.conversations.v1.services.list().then((services: { sid: string; }) => res.send(services)); 
+})
+// Get user servers
+
+// Add participant
+app.get("/add-participant", async (req, res) => {
+  console.log('serversid:' ,req.headers.serversid)
+  console.log('conversationsid: ',req.headers.conversationsid)
+  console.log('identity:' ,req.headers.identity)
+  client.conversations.v1.services(req.headers?.serversid).conversations(req.headers?.conversationsid).participants.create({identity: req.headers?.identity})
+  addToDatabase('members', { user_id: req.headers.uid, server_sid: req.headers?.serversid})
+  addToDatabase('channel_members', { user_id: req.headers?.uid, channel_id: req.headers?.conversationsid })
+  res.status(200).send()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
