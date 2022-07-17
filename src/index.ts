4165bba9a5ab6be3cf4ce536728d72fff7e5c3b4
ikurotime <davidhuertasortiz@gmail.com>
2022-07-18 00:24:10 +0200

    🔧 Change(db)

--- src/index.ts
+++ src/index.ts
@@ -72,9 +72,9 @@ app.get("/create-server", async (req, res) => {
           .conversations(conversation.sid)
           .participants
           .create({identity})
-          await addToDatabase('members', { user_id: req.headers.uid, server_sid: service.sid })
-          await addToDatabase('channels',  { channel_sid: conversation.sid,created_by:req.headers?.uid, server_sid: service.sid,channel_friendly_name: 'general' })
-          addToDatabase('channel_members', { user_id: req.headers.uid, channel_id: conversation.sid,server_sid: service.sid })
+          await addToDatabase('server_members', { user_id: req.headers.uid, server_id: service.sid })
+          await addToDatabase('channels',  { channel_id: conversation.sid,created_by:req.headers?.uid, server_id: service.sid,friendly_name: 'general' })
+          addToDatabase('channel_members', { user_id: req.headers.uid, channel_id: conversation.sid,server_id: service.sid })
         res.send({ serverSid: service.sid, conversation: conversation })
 
         } catch (error) {
@@ -128,8 +128,8 @@ app.get("/get-access-token", async (req, res) => {
 app.get("/get-user-conversations", async (req, res) => {
   const { data } = await supabase
   .from('servers')
-		.select('friendly_name, SID, channels(channel_friendly_name, channel_sid)')
-    .eq('SID', req.headers?.serversid)
+		.select('friendly_name, id, channels(friendly_name, channel_id)')
+    .eq('id', req.headers?.serversid)
   if(data!.length == 0) return res.status(403).send('User is not a member of any server')
   const channels: string[] = [];
   data![0].channels.forEach((channel:{channel_friendly_name:string,channel_sid:string}) => {
@@ -156,8 +156,8 @@ app.get("/add-participant", async (req, res) => {
   console.log('conversationsid: ',req.headers.conversationsid)
   console.log('identity:' ,req.headers.identity)
   client.conversations.v1.services(req.headers?.serversid).conversations(req.headers?.conversationsid).participants.create({identity: req.headers?.identity})
-  addToDatabase('members', { user_id: req.headers.uid, server_sid: req.headers?.serversid})
-  addToDatabase('channel_members', { user_id: req.headers?.uid, channel_id: req.headers?.conversationsid, server_sid: req.headers?.serversid })
+  addToDatabase('server_members', { user_id: req.headers.uid, server_id: req.headers?.serversid})
+  addToDatabase('channel_members', { user_id: req.headers?.uid, channel_id: req.headers?.conversationsid, server_id: req.headers?.serversid })
   res.status(200).send()
 })
 
